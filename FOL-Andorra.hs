610aa710a05836b3d12b58d02ba9aa0b6cc4407e
Arved <arved.friedemann@stoppe.de>
2020-05-27 12:56:18 +0200

    Cleaned up the equality thing to not need the constructors anymore. Removed variables...they might very well not be needed.

--- FOL-Andorra.hs
+++ FOL-Andorra.hs
@@ -6,34 +6,34 @@
 module FOL_Andorra where
 
 import Control.Monad.Var
-
-data Const = TOP | BOT | UNKNOWN
-data Term v a = CONST Const | ATOM a | VAR (PTerm v a) | APPL (PTerm v a) (PTerm v a)
+data Decision = TOP | BOT
+data Const = DEC Decision | UNKNOWN
+data Term v a = CONST Const | ATOM a {-| VAR (PTerm v a) |-} APPL (PTerm v a) (PTerm v a)
 deriving instance (Eq a, Eq (v a), Eq (v (PTerm v a))) => Eq (PTerm v a)
 
 type PTerm v a = v (PTerm v a)
 
---processes a deconstructed equality. Returns conjunction of new constraints, possibly containin the inequality.
+--processes a deconstructed equality. Returns conjunction of new equality constraints with the information whether they hold or not, possibly containin the inequality. Returns [] when constraint holds.
 equality :: (Eq (PTerm v a)) =>
-            (PTerm v a -> PTerm v a -> m (PTerm v a)) ->
-            (PTerm v a -> PTerm v a -> m (PTerm v a)) ->
              PTerm v a ->
             (PTerm v a, Term v a) -> (PTerm v a, Term v a) ->
-            m [PTerm v a]
-equality eqconst ineqconst this (pa, UNKNOWN) (pb, UNKNOWN) = return $ Just [this]
-equality eqconst ineqconst this (pa, UNKNOWN) (pb, b) = --assign pa to b
-equality eqconst ineqconst this (pa, a) (pb, UNKNOWN) = --assign pb to a
-equality eqconst ineqconst this (pa, CONST x) (pb, CONST x')
+            m [(Decision, PTerm v a, PTerm v a)]
+equality (pa, UNKNOWN) (pb, UNKNOWN) = return [(TOP, pa, pb)]
+equality (pa, UNKNOWN) (pb, b) = put pa b >> return []
+equality (pa, a) (pb, UNKNOWN) = put pb a >> return []
+equality (pa, CONST x) (pb, CONST x')
   | x == x' = return []
-  | otherwise = return [ineqconst pa pb]
-equality eqconst ineqconst this (pa, ATOM x) (pb, ATOM x') = --case split on x==x'
+  | otherwise = return [(BOT, pa, pb)]
+equality (pa, ATOM x) (pb, ATOM x') =
   | x == x' = return []
-  | otherwise = return [ineqconst pa pb]
-equality eqconst ineqconst this (pa, VAR x) (pb, VAR x') =
+  | otherwise = return [(BOT, pa, pb)]
+{-
+equality (pa, VAR x) (pb, VAR x') =
   | x == x' = return []
-  | otherwise = return [eqconst x x']
-equality eqconst ineqconst this (pa, VAR x) (pb, _) = return [eqconst x pb]
-equality eqconst ineqconst this (pa, _) (pb, VAR x) = return [eqconst x pa]
+  | otherwise = return [(TOP, pa, pb)]
 
-equality eqconst ineqconst this (pa, APPL px py) (pb, APPL px' py') = return [eqconst px px', eqconst py py']
-equality eqconst ineqconst this (pa, _) (pb, _) = return [ineqconst pa pb]
+equality (pa, VAR x) (pb, _) = return [(TOP, x, pb)]
+equality (pa, _) (pb, VAR x) = return [(TOP, pa, x)]
+-}
+equality (pa, APPL px py) (pb, APPL px' py') = return [(TOP, px, px'), (TOP, py, py')] --not sure if needed. If no constraint is being copied, are the tuples canonical?
+equality (pa, _) (pb, _) = return [(BOT, pa, pb)]
